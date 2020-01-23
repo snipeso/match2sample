155c21ff3d79ada2765da616398548f266c14111
snipeso <code@sophia.science>
2020-01-23 15:27:48 +0100

    new config system

--- config/configMatch2Sample.py
+++ config/configMatch2Sample.py
@@ -1,19 +1,20 @@
 import os
-from config.configSession import CONF
+from config.updateConfig import UpdateConfig
 
 
-CONF.update({
+match2sampleCONF = {
     "task": {
         "name": "match2sample",
-        "blocks": 4,  # number of blocks, try to be even
-        "trials": 10,  # number of trials per condition
-        "levels": [1, 3, 6],
+        "blocks": {"versionMain": 4, "versionDemo": 1, "versionDebug": 1},
+        # number of trials per condition
+        "trials":  {"versionMain": 10, "versionDemo": 30, "versionDebug": 5},
+        "levels":  {"versionMain": [1, 3, 6], "versionDemo": [1], "versionDebug": [1, 3, 6]},
         "stimTime": 2,  # in seconds, min time to be considered a valid RT
         # time window after stimulus disappearance when it still counts as a key response
-        "retentionTime": 4,
+        "retentionTime": {"versionMain": 4, "versionDemo": 1, "versionDebug": 1},
         "probeTime": 3,
-        "answerKeys": ["right", "left", ]  # mismatch and match respectively
-
+        "answerKeys": ["right", "left", ],  # mismatch and match respectively
+        "maxMissed": 3
     },
     "stimuli": {
         "location": os.path.join("stimuli", "jediAlphabet"),
@@ -39,24 +40,20 @@ CONF.update({
     "tones": {
         "alarm": "horn.wav",
     }
-})
+}
 
 
-CONF["screen"]["size"] = CONF["screen"]["size"] if CONF["screen"]["full"] else CONF["screen"]["debugSize"]
-CONF["screen"]["resolution"] = CONF["screen"]["resolution"] if CONF["screen"]["full"] else CONF["screen"]["debugResolution"]
-
-# additional triggers
-CONF["trigger"]["labels"]["StartFix"] = 0x0A
-CONF["trigger"]["labels"]["MatchProbe"] = 0x0B
-CONF["trigger"]["labels"]["NonMatchProbe"] = 0x0C
-CONF["trigger"]["labels"]["CorrectAnswer"] = 0x0D
-CONF["trigger"]["labels"]["IncorrectAnswer"] = 0x0E
+match2sampleTriggers = {
+    "StartFix": 10,
+    "MatchProbe": 11,
+    "NonMatchProbe": 12,
+    "CorrectAnswer": 13,
+    "IncorrectAnswer": 14
+}
 
 
-if CONF["version"] == "demo":
-    CONF["task"]["blocks"] = 1
-    CONF["task"]["trials"] = 30
-    CONF["task"]["levels"] = [1]
-    CONF["task"]["stimTime"] = 1
-    CONF["task"]["retentionTime"] = 1
-    CONF["task"]["probeTime"] = 3
+updateCofig = UpdateConfig()
+updateCofig.addContent(match2sampleCONF)
+updateCofig.addTriggers(match2sampleTriggers)
+
+CONF = updateCofig.getConfig()
