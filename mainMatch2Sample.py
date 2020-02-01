deff276de7c7684bb713278b6198a13864cda0b4
snipeso <code@sophia.science>
2020-02-01 20:47:59 +0100

    new pupil saving order

--- mainMatch2Sample.py
+++ mainMatch2Sample.py
@@ -90,7 +90,7 @@ if CONF["showInstructions"]:
 
 
 eyetracker.start_recording(os.path.join(
-    CONF["participant"], CONF["session"], CONF["task"]["name"]))
+    CONF["participant"], CONF["task"]["name"], CONF["session"]))
 
 # Blank screen for initial rest
 screen.show_blank()
